bdbd1e85f5aba7f569cdde773ad0069cfd01e02b
BestCoderz <utk.bkkg@gmail.com>
2018-08-05 13:52:33 +0530

    Update tutorial1.py

--- tutorial1.py
+++ tutorial1.py
@@ -14,7 +14,7 @@ async def on_ready():
 	return await client.change_presence(game=discord.Game(name='<BOT STATUS HERE>')) #add_your_bot_status_here
 
 @client.command()
-async def ping(*args):
-	await client.say("COMMAND RESPONCE HERE")
+async def ping(*args): #replace_ping_from_any_other_command_name_like_hu,hello,etc
+	await client.say("COMMAND RESPONCE HERE") #replace_responce_with_any_responce_that__you_want_when_command_is_used
 
 client.run('BOT TOKEN HERE') #add_your_bot_token_here
