f78f97b23ef789e1d479631ec3cc0074b11b82c1
BestCoderz <utk.bkkg@gmail.com>
2018-11-25 13:02:42 +0530

    Create welcomer_dm.py

---
+++ welcomer_dm.py
@@ -0,0 +1,26 @@
+import discord
+import asyncio
+from discord.ext import commands
+
+client = commands.Bot(description="MultiVerse Official Bot", command_prefix="mv!", pm_help = True)
+client.remove_command('help')
+
+@client.event
+async def on_ready():
+    print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
+    print('--------')
+    print('--------')
+    print('Started BOT')
+    
+    
+@client.event
+async def on_member_join(member):
+    print("In our server" + member.name + " just joined")
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
+    embed.set_author(name='Welcome message')
+    embed.add_field(name = '__Welcome to Our Server__',value ='**Hope you will be active here. Check Our server rules and never try to break any rules. ',inline = False)
+    embed.set_image(url = 'https://media.giphy.com/media/OkJat1YNdoD3W/giphy.gif')
+    await client.send_message(member,embed=embed)
+    
+client.run('Token')
