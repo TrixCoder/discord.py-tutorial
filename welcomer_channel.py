b915f3a6894c513474eebd40b06cad6d66f3f197
BestCoderz <utk.bkkg@gmail.com>
2018-12-07 12:04:29 +0530

    Update welcomer_channel.py

--- welcomer_channel.py
+++ welcomer_channel.py
@@ -15,19 +15,21 @@ async def on_ready():
 @client.event
 async def on_member_join(member):
     for channel in member.server.channels:
-        if channel.name == '<welcome channel name here>':
+        if channel.name == '<Welcome channel name>':
             r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
-            embed = discord.Embed(title=f'Welcome {member.name} to {member.server.name}', description='Do not forget to check <#491281610503618561> and never try to break any one of them', color = discord.Color((r << 16) + (g << 8) + b))
+            embed = discord.Embed(title=f'Welcome {member.name} to {member.server.name}', description='Do not forget to check rules and never try to break any one of them', color = discord.Color((r << 16) + (g << 8) + b))
             embed.add_field(name='__Thanks for joining__', value='**Hope you will be active here.**', inline=True)
-            embed.add_field(name='Your join position is', value=member.joined_at)
-            embed.set_thumbnail(url=member.avatar_url)
-            await client.send_message(channel, embed=embed)
+            embed.set_thumbnail(url='https://media.giphy.com/media/OkJat1YNdoD3W/giphy.gif') 
+            embed.set_image(url = member.avatar_url)
+            embed.add_field(name='__Join position__', value='{}'.format(str(member.server.member_count)), inline=True)
+            embed.add_field(name='Time of joining', value=member.joined_at)
+            await client.send_message(channel, embed=embed) 
             
             
 @client.event
 async def on_member_remove(member):
     for channel in member.server.channels:
-        if channel.name == '<desired channel name here>':
+        if channel.name == '<Welcome/leave channel name>':
             r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
             embed = discord.Embed(title=f'{member.name} just left {member.server.name}', description='Bye bye 👋! We will miss you 😢', color = discord.Color((r << 16) + (g << 8) + b))
             embed.add_field(name='__User left__', value='**Hope you will be back soon 😕.**', inline=True)
