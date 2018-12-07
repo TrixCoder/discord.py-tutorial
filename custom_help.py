1755fe8931c54e2c7ce3e72ff7de0db1df6cd4c7
BestCoderz <utk.bkkg@gmail.com>
2018-12-07 12:15:41 +0530

    Create custom_help.py

---
+++ custom_help.py
@@ -0,0 +1,28 @@
+import discord
+import asyncio
+from discord.ext.commands import Bot
+
+client = Bot(description="<bot description>", command_prefix="d!", pm_help = True)
+client.remove_command('help')
+
+@client.event
+async def on_ready():
+    print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
+    print('--------')
+    print('--------')
+    print('Started BOT')
+    
+@client.command(pass_context = True)
+async def help(ctx):
+    author = ctx.message.author
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
+    embed.set_author(name='Help')
+    embed.set_image(url = 'https://image.ibb.co/caM2BK/help.gif')
+    embed.add_field(name = '``Our Help Server Link`` ',value ='https://discord.gg/vMvv5rr',inline = False)
+    embed.add_field(name = 'd!modhelp ',value ='Explaines all the commands which are only usable by Those who has moderation permissions. Like- Manage Nicknames, Manage Messages, Kick/Ban Members,etc.',inline = False)
+    embed.add_field(name = 'd!generalhelp ',value ='Explaines all the commands which are usable by everyone.',inline = False)
+    await client.send_message(author,embed=embed)
+    await client.say('📨 Check DMs For Information')
+    
+client.run('Token')
