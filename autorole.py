993a64689e6d99fcf826d35ae76986254eeee9f3
BestCoderz <utk.bkkg@gmail.com>
2018-12-07 12:44:24 +0530

    Update autorole.py

--- autorole.py
+++ autorole.py
@@ -1,4 +1,4 @@
 @client.event
 async def on_member_join(member):
-    role = discord.utils.get(member.server.roles, name='<rolename>')
+    role = discord.utils.get(member.server.roles, name='rolename here')
     await client.add_roles(member, role)
