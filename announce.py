de056c4a9ebfb90457acb3aacbbf42882f2ffbc7
BestCoderz <utk.bkkg@gmail.com>
2018-12-07 12:28:01 +0530

    Create announce.py

---
+++ announce.py
@@ -0,0 +1,16 @@
+import colorsys
+import random
+
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+@client.command(pass_context = True)
+@commands.has_permissions(administrator=True) 
+async def announce(ctx, channel: discord.Channel=None, *, msg: str):
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    embed=discord.Embed(title="Announcement", description="{}".format(msg), color = discord.Color((r << 16) + (g << 8) + b))
+    await client.send_message(channel, embed=embed)
+    await client.delete_message(ctx.message)
+    
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+client.run('Token')
