8534078c31614c02fa80ab58bcde3c9d86d25efc
BestCoderz <utk.bkkg@gmail.com>
2018-12-21 12:26:18 +0530

    Update basicbot.py

--- basicbot.py
+++ basicbot.py
@@ -13,7 +13,7 @@ async def on_ready():
 	print('Started <BOTNAME HERE>') #add_your_bot_name_here
 	return await client.change_presence(game=discord.Game(name='<BOT STATUS HERE>')) #add_your_bot_status_here
 
-@client.command()
+@client.command(pass_context=True)
 async def ping(ctx): #replace_ping_from_any_other_command_name_like_hu,hello,etc
 	await client.say("COMMAND RESPONCE HERE") #replace_responce_with_any_responce_that__you_want_when_command_is_used
 
